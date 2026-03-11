617ea94857b4d1f46f38d2cd9b1ed8073ed34e14
wangfiox <wangfiox@gmail.com>
2026-03-11 20:53:16 +0800

    x86

--- darwin.nix
+++ darwin.nix
@@ -5,4 +5,11 @@
 
   # 允许使用 Touch ID 进行 sudo 认证
   security.pam.services.sudo_local.touchIdAuth = true;
+
+  # 启用 x86_64 跨平台支持（Rosetta 2）
+  # 因为使用 Determinate Nix Installer（nix.enable = false），
+  # 通过写入额外的 nix 配置文件来补充 extra-platforms 设置
+  environment.etc."nix/nix.custom.conf".text = ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
 }
